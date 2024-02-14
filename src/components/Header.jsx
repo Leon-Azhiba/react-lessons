d29dcefdd7a696137b514ac28827927006109f0d
Leon-Azhiba <leon.azhiba.work@gmail.com>
2024-02-14 18:39:32 +0300

    changed navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.clipartmax.com/png/full/246-2461139_selling-realmpvp-hcf-server-logo-minecraft-hcf.png'></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div className='bg'>
+                <img src='https://discordhome.com/user_upload/backgrounds/18437background.jpg' />
+            </div>
+            <div>ava+description</div>
+            <div>
+                <h3>My posts</h3>
+                <div>new posts</div>
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
